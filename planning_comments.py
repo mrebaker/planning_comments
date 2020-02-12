5afaf4928b304dacbec8451b151ef99d75518ede
onskar <matt.baker988@gmail.com>
2020-02-12 07:12:22 +0000

    Now loops through all pages of comments

--- planning_comments.py
+++ planning_comments.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import csv
 import requests
+import time
 
 base_url = 'https://planning.n-somerset.gov.uk/online-applications/applicationDetails.do?activeTab=neighbourComments&keyVal=PJML85LPMKI00&neighbourCommentsPager'
+
+
+def write_to_csv(result_list):
+    writer = csv.DictWriter(open('comments.csv', 'w+'),
+                            fieldnames=['address', 'stance', 'date_submitted', 'text'],
+                            newline='')
+    writer.writeheader()
+    writer.writerows(result_list)
+
 
 if __name__ == '__main__':
     p = 1
@@ -10,7 +20,10 @@ if __name__ == '__main__':
     while True:
         page = requests.get(base_url + f'.page={p}')
         if page.status_code != 200:
+            if results:
+                write_to_csv(results)
             page.raise_for_status()
+
         soup = BeautifulSoup(page.text, "html.parser")
         comments = soup.find_all("div", class_="comment")
         if comments:
@@ -21,9 +34,9 @@ if __name__ == '__main__':
                                 'date_submitted': " ".join(comment.select_one('div h4', class_='commentText').text.split()),
                                 'text': comment.select_one('div p', class_='commentText').text.strip()
                                 })
-            writer = csv.DictWriter(open('comments.csv', 'w+'), ['address', 'stance', 'date_submitted', 'text'])
-            writer.writerows(results)
-            exit(0)
             p += 1
+            time.sleep(2)
+
         else:
+            write_to_csv(results)
             break
